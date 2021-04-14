bfccd9b6a90b9b40b8e4a054c1d5d69a32010856
Dinoryong <biiregrey2@gmail.com>
2021-04-14 22:50:16 +0900

    Updates

---
+++ component---src-pages-404-js-dcb7a3e9d13de974ae5b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(t,e,a){"use strict";a.r(e);var n=a("q1tI"),o=a.n(n),l=a("Zttt"),i=a("vrFN");e.default=function(t){var e=t.data.site.siteMetadata.title,a=t.location;return o.a.createElement(l.a,{location:a,title:e},o.a.createElement(i.a,{title:"404: Not Found"}),o.a.createElement("h1",null,"404: Not Found"),o.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-dcb7a3e9d13de974ae5b.js.map
